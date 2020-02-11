2e09c085ed86e39db6648b7a3260c1ebc330eed6
Anthony Chan <appianchan@gmail.com>
2020-02-10 18:48:43 -0800

    solving code

--- First-Set.js
+++ First-Set.js
@@ -1,7 +1,21 @@
+// Determine whether an integer is a palindrome.An integer is a palindrome when it reads the same backward as forward.
+
+
 var isPalindrome = function (x) {
-    if(x < 0){
+    if (x < 0) {
+        return false;
+    }
+    var n = x.toString();
+    var y = n.split("");
+    
+    if (y === y.reverse()) {
+        console.log(y);
+        console.log(y.reverse());
+        return true;
+    } else {
+        
         return false;
     }
 };
 
-isPalindrome(121);
+isPalindrome(10);
