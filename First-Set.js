5eeb80fa4e2dcaddc2e50ea56a740b8ed252559d
Anthony Chan <appianchan@gmail.com>
2020-02-14 19:32:24 -0800

    solved Roman to Integer problem

--- First-Set.js
+++ First-Set.js
@@ -48,3 +48,77 @@ var searchInsert = function (nums, target) {
 
 };
 
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII.Instead, the number four is written as IV.Because the one is before the five we subtract it making four.The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
+
+// I can be placed before V(5) and X(10) to make 4 and 9.
+// X can be placed before L(50) and C(100) to make 40 and 90.
+// C can be placed before D(500) and M(1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.Input is guaranteed to be within the range from 1 to 3999.
+var romanToInt = function (s) {
+    var reference = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
+    var final = 0;
+    var x = 0;
+    while (x < s.length) {
+        var numeral = s.charAt(x);
+        if (numeral === 'I') {
+            if (s.charAt(x + 1) === 'V') {
+                final = final + 4;
+                x = x + 2;
+                continue;
+            } else if (s.charAt(x + 1) === 'X') {
+                final = final + 9;
+                x = x + 2;
+                continue;
+            } else {
+                final = final + 1;
+                x = x + 1;
+            }
+        } else if (numeral === 'X') {
+            if (s.charAt(x + 1) === 'L') {
+                final = final + 40;
+                x = x + 2;
+                continue;
+            } else if (s.charAt(x + 1) === 'C') {
+                final = final + 90;
+                x = x + 2;
+                continue;
+            } else {
+                final = final + 10;
+                x = x + 1;
+            }
+        } else if (numeral === 'C') {
+            if (s.charAt(x + 1) === 'D') {
+                final = final + 400;
+                x = x + 2;
+                continue;
+            } else if (s.charAt(x + 1) === 'M') {
+                final = final + 900;
+                x = x + 2;
+                continue;
+            } else {
+                final = final + 100;
+                x = x + 1;
+            }
+        } else {
+            final = final + reference[numeral];
+            x = x + 1;
+        }
+    }
+    return final;
+    
+    
+};
+
+
