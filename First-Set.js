3bc953112686a01d40b5115db32b7da0162e9747
Anthony Chan <appianchan@gmail.com>
2020-02-18 19:22:36 -0800

    uniqueOccurences

--- First-Set.js
+++ First-Set.js
@@ -413,3 +413,24 @@ var uniqueOccurrences = function (arr) {
 
 };
 uniqueOccurrences([1, 2, 2, 1, 1, 3]);
+
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+
+
+//     Example 1:
+
+// Input: arr = [17, 18, 5, 4, 6, 1]
+// Output: [18, 6, 6, 6, 1, -1]
+
+
+// Constraints:
+
+// 1 <= arr.length <= 10 ^ 4
+// 1 <= arr[i] <= 10 ^ 5
+
+var replaceElements = function (arr) {
+
+};
