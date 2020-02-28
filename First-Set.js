e04c0dbd020135a68bd5bd45acc3521c753a3c2b
Anthony Chan <appianchan@gmail.com>
2020-02-27 23:55:39 -0800

    more leetcode problems

--- First-Set.js
+++ First-Set.js
@@ -816,3 +816,32 @@ var rotatedDigits = function (N) {
 
     // }
 };
+
+// We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+
+//     Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+//         Example:
+// Input: 28
+// Output: True
+// Explanation: 28 = 1 + 2 + 4 + 7 + 14
+// Note: The input number n will not exceed 100, 000, 000. (1e8)
+
+var checkPerfectNumber = function (num) {
+    if (num === 0) {
+        return false;
+    }
+    var arr = [];
+    var final = 0;
+    for (var x = 1; x < num; x++) {
+        if (num % x === 0) {
+            arr.push(x);
+        }
+    }
+    for (var x = 0; x < arr.length; x++) {
+        final += arr[x];
+    }
+    if (final === num) {
+        return true;
+    }
+    return false;
+};
