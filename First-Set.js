477471916b4af8bb77243c50a468290adca6e955
Anthony Chan <appianchan@gmail.com>
2020-02-29 23:40:18 -0800

    doing more leetcode problems

--- First-Set.js
+++ First-Set.js
@@ -942,5 +942,33 @@ var containsNearbyDuplicate = function (nums, k) {
 // the decimal part is truncated, 2 is returned.
 
 var mySqrt = function (x) {
+    var y = -1;
+    var counter = 1;
+    if (x === 1) {
+        return 1;
+    }
+    while (y < x) {
+        if (counter * counter > x) {
+            return counter - 1;
+        }
+        counter++;
+    }
+};
+
+// Given a non - negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+
+// Example 1:
+
+// Input: 5
+// Output: True
+// Explanation: 1 * 1 + 2 * 2 = 5
+
+
+// Example 2:
+
+// Input: 3
+// Output: False
+
+var judgeSquareSum = function (c) {
 
 };
