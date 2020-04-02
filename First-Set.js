1b7c4b62afd44458ba0af8e52dab6069f866773e
Anthony Chan <appianchan@gmail.com>
2020-04-01 19:17:48 -0700

    leetcode work

--- First-Set.js
+++ First-Set.js
@@ -1361,3 +1361,14 @@ var sortArrayByParityII = function (A) {
     }
     return final;
 };
+
+// Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+
+//     Example 1:
+
+// Input: S = "loveleetcode", C = 'e'
+// Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+
+var shortestToChar = function (S, C) {
+
+};
