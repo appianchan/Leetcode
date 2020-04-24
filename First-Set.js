c1a1eba50ec494630d344d7b6b696b442824be25
Anthony Chan <appianchan@gmail.com>
2020-04-24 16:56:28 -0700

    Javascript problem

--- First-Set.js
+++ First-Set.js
@@ -1778,3 +1778,52 @@ var maxArea = function (height) {
     }
     return highest;
 };
+
+// Given a date, return the corresponding day of the week for that date.
+
+// The input is given as three integers representing the day, month and year respectively.
+
+// Return the answer as one of the following values { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" }.
+
+
+
+// Example 1:
+
+// Input: day = 31, month = 8, year = 2019
+// Output: "Saturday"
+// Example 2:
+
+// Input: day = 18, month = 7, year = 1999
+// Output: "Sunday"
+// Example 3:
+
+// Input: day = 15, month = 8, year = 1993
+// Output: "Sunday"
+
+var dayOfTheWeek = function (day, month, year) {
+    var date = new Date(month + " " + day + ", " + year + " 00:00:00");
+    var num = date.getDay();
+    if (num === 0) {
+        return "Sunday";
+    }
+    if (num === 1) {
+        return "Monday";
+    }
+    if (num === 2) {
+        return "Tuesday";
+    }
+    if (num === 3) {
+        return "Wednesday";
+    }
+    if (num === 4) {
+        return "Thursday";
+    }
+    if (num === 5) {
+        return "Friday";
+    }
+    if (num === 6) {
+        return "Saturday";
+    }
+
+
+};
