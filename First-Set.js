d7335248be45543d0c855fa3c13a81b3bc22970a
Anthony Chan <appianchan@gmail.com>
2020-04-24 16:08:44 -0700

    working on problems

--- First-Set.js
+++ First-Set.js
@@ -1664,3 +1664,40 @@ var smallestRangeI = function (A, K) {
     return num;
 
 };
+
+// ZigZag Conversion
+// Medium
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+//     P   A   H   N
+// A P L S I I G
+// Y   I   R
+
+// And then read line by line: "PAHNAPLSIIGYIR"
+// Write the code that will take a string and make this conversion given a number of rows:
+// string convert(string s, int numRows);
+// Example 1:
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Example 2:
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+var zigzag = function(string, numRows){
+    var z = "";
+    for (var y = 0; y < numRows; y++) {
+        Var a = y;
+        while (y < string.length) {
+            Z += string[y];
+            Y = y + (numRows - 1) + (numRows - 1);
+        }
+        Y = a;
+    }
+    Return z;
+
+}
