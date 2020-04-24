796b257689f73c64a3fab4b1c1189601bd7b4b6f
Anthony Chan <appianchan@gmail.com>
2020-04-23 23:09:21 -0700

    leetcode work

--- First-Set.js
+++ First-Set.js
@@ -1580,6 +1580,35 @@ var reverseWords = function (s) {
     }
     return y.join(" ");
 };
+// Given an array of integers arr, a lucky integer is an integer which has a frequency in the array equal to its value.
+
+// Return a lucky integer in the array.If there are multiple lucky integers return the largest of them.If there is no lucky integer return -1.
+
+
+
+// Example 1:
+
+// Input: arr = [2, 2, 3, 4]
+// Output: 2
+// Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+// Example 2:
+
+// Input: arr = [1, 2, 2, 3, 3, 3]
+// Output: 3
+// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+//     Example 3:
+
+// Input: arr = [2, 2, 2, 3, 3]
+// Output: -1
+// Explanation: There are no lucky numbers in the array.
+//     Example 4:
+
+// Input: arr = [5]
+// Output: -1
+// Example 5:
+
+// Input: arr = [7, 7, 7, 7, 7, 7, 7]
+// Output: 7
 
 var findLucky = function (arr) {
     var obj = {};
@@ -1597,5 +1626,41 @@ var findLucky = function (arr) {
             return key;
         }
     }
-    return obj[2];
+    return -1;
+};
+
+// Given an array A of integers, for each integer A[i] we may choose any x with -K <= x <= K, and add x to A[i].
+
+// After this process, we have some array B.
+
+// Return the smallest possible difference between the maximum value of B and the minimum value of B.
+
+
+
+//     Example 1:
+
+// Input: A = [1], K = 0
+// Output: 0
+// Explanation: B = [1]
+// Example 2:
+
+// Input: A = [0, 10], K = 2
+// Output: 6
+// Explanation: B = [2, 8]
+// Example 3:
+
+// Input: A = [1, 3, 6], K = 3
+// Output: 0
+// Explanation: B = [3, 3, 3] or B = [4, 4, 4]
+var smallestRangeI = function (A, K) {
+    if (A.length === 1) {
+        return 0;
+    }
+    A.sort((a, b) => a - b)
+    var num = A[A.length - 1] - A[0] - K * 2;
+    if (num < 0) {
+        return 0;
+    }
+    return num;
+
 };
