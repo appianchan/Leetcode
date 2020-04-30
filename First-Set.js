3aaadb674fcb9ba8adea0ac94e81ce0c231b43c0
Anthony Chan <appianchan@gmail.com>
2020-04-29 22:39:15 -0700

    anagram problem

--- First-Set.js
+++ First-Set.js
@@ -2079,3 +2079,61 @@ var findDuplicate = function (nums) {
         }
     }
 };
+// Given a string s and a non - empty string p, find all the start indices of p's anagrams in s.
+
+// Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20, 100.
+
+// The order of output does not matter.
+
+//     Example 1:
+
+// Input:
+// s: "cbaebabacd" p: "abc"
+
+// Output:
+// [0, 6]
+
+// Explanation:
+// The substring with start index = 0 is "cba", which is an anagram of "abc".
+// The substring with start index = 6 is "bac", which is an anagram of "abc".
+//     Example 2:
+
+// Input:
+// s: "abab" p: "ab"
+
+// Output:
+// [0, 1, 2]
+
+// Explanation:
+// The substring with start index = 0 is "ab", which is an anagram of "ab".
+// The substring with start index = 1 is "ba", which is an anagram of "ab".
+// The substring with start index = 2 is "ab", which is an anagram of "ab".
+var findAnagrams = function (s, p) {
+    var final = [];
+    var anagrams = {};
+    for (var x = 0; x < p.length; x++) {
+        var letter = p[x];
+        if (!anagrams[letter]) {
+            anagrams[letter] = 1
+        } else {
+            anagrams[letter] += 1
+        }
+    }
+
+    for (var y = 0; y < s.length - p.length; y++) {
+        var anagrams2 = {};
+        for (var z = y; z < y + p.length; z++) {
+            var letter = s[z];
+            if (!anagrams2[letter]) {
+                anagrams2[letter] = 1
+            } else {
+                anagrams2[letter] += 1
+            }
+        }
+        if (JSON.stringify(anagrams) === JSON.stringify(anagrams2)) {
+            final.push(y);
+
+        }
+    }
+    return final;
+};
