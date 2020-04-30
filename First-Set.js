5156c381bed6cf6eebbcb9ad0b9e5226947cd152
Anthony Chan <appianchan@gmail.com>
2020-04-29 22:45:03 -0700

    more javascript

--- First-Set.js
+++ First-Set.js
@@ -2137,3 +2137,37 @@ var findAnagrams = function (s, p) {
     }
     return final;
 };
+
+// Given an array of integers, find if the array contains any duplicates.
+
+// Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+//     Example 1:
+
+// Input: [1, 2, 3, 1]
+// Output: true
+// Example 2:
+
+// Input: [1, 2, 3, 4]
+// Output: false
+// Example 3:
+
+// Input: [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+// Output: true
+var containsDuplicate = function (nums) {
+    var obj = {};
+    for (var x = 0; x < nums.length; x++) {
+        var num = nums[x];
+        if (!obj[num]) {
+            obj[num] = 1
+        } else {
+            obj[num] += 1
+        }
+    }
+    for (var key in obj) {
+        if (obj[key] >= 2) {
+            return true
+        }
+    }
+    return false;
+};
