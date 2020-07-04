cbc50eb5e886b22c140c74e5442ba3343a552cab
Anthony Chan <appianchan@gmail.com>
2020-07-04 13:45:02 -0700

    candy problem

--- First-Set.js
+++ First-Set.js
@@ -2579,3 +2579,11 @@ var maxProduct = function (nums) {
     var x = nums.length - 1;
     return (nums[x] - 1) * (nums[x - 1] - 1);
 };
+
+// Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+// For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them.Notice that multiple kids can have the greatest number of candies.
+
+var kidsWithCandies = function (candies, extraCandies) {
+
+};
