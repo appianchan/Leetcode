e35a0c6bbd76a84baf55fd336a550af1b3d17522
Anthony Chan <appianchan@gmail.com>
2020-07-04 13:50:57 -0700

    candy problem done

--- First-Set.js
+++ First-Set.js
@@ -2585,5 +2585,20 @@ var maxProduct = function (nums) {
 // For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them.Notice that multiple kids can have the greatest number of candies.
 
 var kidsWithCandies = function (candies, extraCandies) {
+    var final = [];
+    var largest = 0;
+    for (var x = 0; x < candies.length; x++) {
+        if (largest < candies[x]) {
+            largest = candies[x];
+        }
+    }
+    for (var x = 0; x < candies.length; x++) {
+        if (candies[x] + extraCandies >= largest) {
+            final.push(true);
+        } else {
+            final.push(false);
+        }
 
+    }
+    return final;
 };
