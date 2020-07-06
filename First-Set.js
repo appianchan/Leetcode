c11d74807aced6d34a1aa5e056999ddece5340ad
Anthony Chan <appianchan@gmail.com>
2020-07-06 14:17:44 -0700

    ugly number finished

--- First-Set.js
+++ First-Set.js
@@ -2675,3 +2675,28 @@ var toLowerCase = function (str) {
 // Input: 14
 // Output: false
 // Explanation: 14 is not ugly since it includes another prime factor 7.
+var isUgly = function (num) {
+    if (num <= 0) {
+        return false;
+    }
+    if (num === 1) {
+        return true;
+    }
+    var number = [];
+    while (num !== 2 && num !== 3 && num !== 5) {
+        if (num % 2 === 0) {
+            num = num / 2;
+            continue;
+        }
+        if (num % 3 === 0) {
+            num /= 3;
+            continue;
+        }
+        if (num % 5 === 0) {
+            num /= 5;
+            continue;
+        }
+        return false;
+    }
+    return true;
+};
