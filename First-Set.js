82bd22277edffb15d64b28cefebd69fa869ffd04
Anthony Chan <appianchan@gmail.com>
2020-07-09 21:32:51 -0700

    height checker problem

--- First-Set.js
+++ First-Set.js
@@ -2825,3 +2825,13 @@ var sortArrayByParity = function (A) {
     }
     return arr;
 };
+
+// Students are asked to stand in non - decreasing order of heights for an annual photo.
+
+// Return the minimum number of students that must move in order for all students to be standing in non - decreasing order of height.
+
+// Notice that when a group of students is selected they can reorder in any possible way between themselves and the non selected students remain on their seats.
+
+var heightChecker = function (heights) {
+
+};
