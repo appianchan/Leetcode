7da42af57bc8f815d9dc5584221f12ad21c6ec3a
Anthony Chan <appianchan@gmail.com>
2020-07-15 20:19:17 -0700

    ransom note problem

--- First-Set.js
+++ First-Set.js
@@ -2927,3 +2927,22 @@ var constructRectangle = function (area) {
     }
     return final_answer;
 };
+
+// Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines; otherwise, it will return false.
+
+// Each letter in the magazine string can only be used once in your ransom note.
+
+
+
+//     Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
