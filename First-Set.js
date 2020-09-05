179451ffd4e46e0512f16238db16ef891f300ce6
Anthony Chan <appianchan@gmail.com>
2020-09-04 17:43:27 -0700

    sort done

--- First-Set.js
+++ First-Set.js
@@ -3436,3 +3436,7 @@ var shiftGrid = function (grid, k) {
 
 // Input: nums = [5,1,1,2,0,0]
 // Output: [0,0,1,1,2,5]
+
+var sortArray = function (nums) {
+    return nums.sort(function (a, b) { return a - b })
+};
