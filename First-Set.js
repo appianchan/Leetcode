b40899cf928e0ae0f1aacb43d0e867974448b0e9
Anthony Chan <appianchan@gmail.com>
2020-09-27 20:39:44 -0700

    finished reverse array problem

--- First-Set.js
+++ First-Set.js
@@ -3701,3 +3701,64 @@ var countGoodTriplets = function (arr, a, b, c) {
 
 // Input: target = [1,1,1,1,1], arr = [1,1,1,1,1]
 // Output: true
+var canBeEqual = function (target, arr) {
+    var obj = {};
+    for (var x = 0; x < target.length; x++) {
+        var num = target[x];
+        if (!obj[num]) {
+            obj[num] = 1;
+        } else {
+            obj[num] += 1;
+        }
+    }
+    for (var x = 0; x < arr.length; x++) {
+        var num = arr[x];
+        if (!obj[num]) {
+            return false;
+        } else {
+            obj[num] -= 1;
+        }
+    }
+    var final = Object.values(obj);
+    for (var x = 0; x < final.length; x++) {
+        var num = final[x];
+        if (num !== 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+// Given an array of positive integers arr, calculate the sum of all possible odd - length subarrays.
+
+// A subarray is a contiguous subsequence of the array.
+
+// Return the sum of all odd - length subarrays of arr.
+
+
+
+//     Example 1:
+
+// Input: arr = [1, 4, 2, 5, 3]
+// Output: 58
+// Explanation: The odd - length subarrays of arr and their sums are:
+// [1] = 1
+// [4] = 4
+// [2] = 2
+// [5] = 5
+// [3] = 3
+// [1, 4, 2] = 7
+// [4, 2, 5] = 11
+// [2, 5, 3] = 10
+// [1, 4, 2, 5, 3] = 15
+// If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+// Example 2:
+
+// Input: arr = [1, 2]
+// Output: 3
+// Explanation: There are only 2 subarrays of odd length, [1] and[2].Their sum is 3.
+// Example 3:
+
+// Input: arr = [10, 11, 12]
+// Output: 66
+
