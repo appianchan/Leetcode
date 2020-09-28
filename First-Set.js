f338c22690df15f8a06d844d5fc74f8d8ba410dd
Anthony Chan <appianchan@gmail.com>
2020-09-27 20:29:30 -0700

    reverse array problem

--- First-Set.js
+++ First-Set.js
@@ -3633,6 +3633,16 @@ var busyStudent = function (startTime, endTime, queryTime) {
 // Where | x | denotes the absolute value of x.
 
 // Return the number of good triplets.
+//     Example 1:
+
+// Input: arr = [3, 0, 1, 1, 9, 7], a = 7, b = 2, c = 3
+// Output: 4
+// Explanation: There are 4 good triplets: [(3, 0, 1), (3, 0, 1), (3, 1, 1), (0, 1, 1)].
+//     Example 2:
+
+// Input: arr = [1, 1, 2, 2, 3], a = 0, b = 0, c = 1
+// Output: 0
+// Explanation: No triplet satisfies all conditions.
 
 var countGoodTriplets = function (arr, a, b, c) {
     var counter = 0;
@@ -3654,13 +3664,40 @@ var countGoodTriplets = function (arr, a, b, c) {
 
 
 
-//     Example 1:
 
-// Input: arr = [3, 0, 1, 1, 9, 7], a = 7, b = 2, c = 3
-// Output: 4
-// Explanation: There are 4 good triplets: [(3, 0, 1), (3, 0, 1), (3, 1, 1), (0, 1, 1)].
-//     Example 2:
 
-// Input: arr = [1, 1, 2, 2, 3], a = 0, b = 0, c = 1
-// Output: 0
-// Explanation: No triplet satisfies all conditions.
+// Given two integer arrays of equal length target and arr.
+
+// In one step, you can select any non-empty sub-array of arr and reverse it. You are allowed to make any number of steps.
+
+// Return True if you can make arr equal to target, or False otherwise.
+
+ 
+
+// Example 1:
+
+// Input: target = [1,2,3,4], arr = [2,4,1,3]
+// Output: true
+// Explanation: You can follow the next steps to convert arr to target:
+// 1- Reverse sub-array [2,4,1], arr becomes [1,4,2,3]
+// 2- Reverse sub-array [4,2], arr becomes [1,2,4,3]
+// 3- Reverse sub-array [4,3], arr becomes [1,2,3,4]
+// There are multiple ways to convert arr to target, this is not the only way to do so.
+// Example 2:
+
+// Input: target = [7], arr = [7]
+// Output: true
+// Explanation: arr is equal to target without any reverses.
+// Example 3:
+
+// Input: target = [1,12], arr = [12,1]
+// Output: true
+// Example 4:
+
+// Input: target = [3,7,9], arr = [3,7,11]
+// Output: false
+// Explanation: arr doesn't have value 9 and it can never be converted to target.
+// Example 5:
+
+// Input: target = [1,1,1,1,1], arr = [1,1,1,1,1]
+// Output: true
