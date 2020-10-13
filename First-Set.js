85e7cc5b8a2b1c94650b18c208c44e0940aa2bcb
Anthony Chan <appianchan@gmail.com>
2020-10-12 19:05:14 -0700

    sort character frequency problem

--- First-Set.js
+++ First-Set.js
@@ -4485,7 +4485,6 @@ ORDER BY id ASC;
 // Input: nums = [-2147483647]
 // Output: -2147483647
 
- * /
 var maxSubArray = function (nums) {
     var prev = 0;
     var max = -Number.MAX_VALUE;
@@ -4496,3 +4495,39 @@ var maxSubArray = function (nums) {
     }
     return max;
 };
+
+// Given a string, sort it in decreasing order based on the frequency of characters.
+
+//     Example 1:
+
+// Input:
+// "tree"
+
+// Output:
+// "eert"
+
+// Explanation:
+// 'e' appears twice while 'r' and 't' both appear once.
+//     So 'e' must appear before both 'r' and 't'.Therefore "eetr" is also a valid answer.
+//         Example 2:
+
+// Input:
+// "cccaaa"
+
+// Output:
+// "cccaaa"
+
+// Explanation:
+// Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+//     Example 3:
+
+// Input:
+// "Aabb"
+
+// Output:
+// "bbAa"
+
+// Explanation:
+// "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
