c86971bd0b4790c14e7e3fe31aae40661b9d93d2
Anthony Chan <appianchan@gmail.com>
2020-10-12 19:02:39 -0700

    largest subarray problem solved

--- First-Set.js
+++ First-Set.js
@@ -4484,3 +4484,15 @@ ORDER BY id ASC;
 
 // Input: nums = [-2147483647]
 // Output: -2147483647
+
+ * /
+var maxSubArray = function (nums) {
+    var prev = 0;
+    var max = -Number.MAX_VALUE;
+
+    for (var i = 0; i < nums.length; i++) {
+        prev = Math.max(prev + nums[i], nums[i]);
+        max = Math.max(max, prev);
+    }
+    return max;
+};
