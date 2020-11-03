6db9902cd9eed65000f9fbd6d025cf80fb504abe
Anthony Chan <appianchan@gmail.com>
2020-11-02 19:56:44 -0800

    SQL solution

--- First-Set.js
+++ First-Set.js
@@ -4692,3 +4692,6 @@ var lastStoneWeight = function (stones) {
 // | Algeria      | 37100000    | 2381741      |
 // +--------------+-------------+--------------+
 
+SELECT name,population,area
+FROM World
+WHERE area>3000000 OR population>25000000
